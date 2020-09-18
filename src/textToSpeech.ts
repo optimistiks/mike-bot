88c39976951404a3f2653c7467784969a263ac2a
optimistiks <mail@potapovmax.com>
2020-09-19 01:17:55 +0300

    revert: change back from ssml to plain text

--- src/textToSpeech.ts
+++ src/textToSpeech.ts
@@ -3,11 +3,10 @@ import AWS, { Polly } from "aws-sdk";
 const polly = new AWS.Polly();
 
 export async function textToSpeech(text: string): Promise<Polly.AudioStream> {
-  const ssml = `<speak>${text}</speak>`;
   const audioBuffer = await new Promise<Polly.AudioStream>(
     (resolve, reject) => {
       polly.synthesizeSpeech(
-        { Text: ssml, TextType: "ssml", VoiceId: "Maxim", OutputFormat: "mp3" },
+        { Text: text, VoiceId: "Maxim", OutputFormat: "mp3" },
         (err, data) => {
           if (err) {
             return reject(err);
